7a66e58ba7c0679b1d532b77947fc8b0fc7a1666
Kyle VanderBeek <kyle@change.org>
2013-01-17 20:10:11 -0800

    rails new ~/Work/fresh32

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fresh32::Application.config.secret_token = '81d251c8d06b01e4c726bd7285c4f3d2308ff920827bab5e8a53907eac7afe86d87bc051d1e57a8abf59a7e231eb1a43d9fa5128a8107ca39f952706b98c09b5'
